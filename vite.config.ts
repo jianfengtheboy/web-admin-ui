fe86968f907be2d69ac648ed5bbc705dfe692e8e
jianfengtheboy <jianfengtheboy@163.com>
2022-05-27 23:40:08 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+  plugins: [vue()]
+})
