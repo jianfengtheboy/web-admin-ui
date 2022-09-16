37e9db2ebee83225b56bc8b15c976a0613167172
jianfengtheboy <jianfengtheboy@163.com>
2022-09-16 21:22:58 +0800

    清空分支

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { ElConfigProvider } from 'element-plus'
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
-</script>
-
-<template>
-  <el-config-provider :locale="zhCn"> </el-config-provider>
-</template>
--- src/model/common.ts
+++
@@ -1,5 +0,0 @@
-export interface IUser {
-  id: number
-  // 用户名
-  userName: string
-}
